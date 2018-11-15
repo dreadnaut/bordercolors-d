65e09c03f55937b88ee67f3c52c34f52bf549406
dreadnaut <dreadnaut@gmail.com>
2018-11-15 23:15:11 +0000

    Import sources from BorderColors-GT 1.0.8

---
+++ chrome/content/options.js
@@ -0,0 +1,53 @@
+﻿function init(){
+	var consoleService = Components.classes["@mozilla.org/consoleservice;1"].getService(Components.interfaces.nsIConsoleService);
+	var properties = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService);
+	var myProperties = properties.getBranch("extensions.borderColorsGT.");
+	var accountManager = Components.classes["@mozilla.org/messenger/account-manager;1"].getService(Components.interfaces.nsIMsgAccountManager);
+	var accountsList = accountManager.accounts;
+	for (var i = 0; i < accountsList.length; i++) {
+		var nextAccount = accountsList.queryElementAt(i, Components.interfaces.nsIMsgAccount);
+		var identitiesList = nextAccount.identities;
+        for (var j = 0; j < identitiesList.length; j++) {
+			var nextIdentity = identitiesList.queryElementAt(j, Components.interfaces.nsIMsgIdentity);
+			var  identityProperty = myProperties.getChildList("colors."+nextIdentity.identityName);
+			if(identityProperty.length===0){
+				myProperties.setCharPref("colors."+nextIdentity.identityName,"#FFFFFF");
+			}
+			var newHbox = document.createElement("hbox");
+			var newLabel = document.createElement("label");
+			var newSeparator = document.createElement("separator");
+			var newColorPicker = document.createElement("colorpicker");
+			newLabel.setAttribute("value", nextIdentity.identityName);
+			newHbox.setAttribute("id", nextIdentity.identityName);
+			newSeparator.setAttribute("orient","vertical");
+			var container = document.getElementById('colorSelectors');
+			newColorPicker.setAttribute("type","button");
+			newColorPicker.setAttribute("color",myProperties.getCharPref("colors."+nextIdentity.identityName));
+			newHbox.appendChild(newColorPicker);
+			newHbox.appendChild(newSeparator);
+			newHbox.appendChild(newLabel);
+			container.appendChild(newHbox);
+		}
+	}
+	var borderWidth =  myProperties.getChildList("borderWidth");
+	size = document.getElementById('BorderWidth');
+	if(borderWidth.length===0){
+		size.selectedIndex = 0;
+	}
+	else{
+		size.selectedIndex = myProperties.getCharPref("borderWidth") - 1;
+	}
+}
+
+function accept(){
+	var properties = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService);
+	var myProperties = properties.getBranch("extensions.borderColorsGT.");
+	var colorList = document.getElementById("colorSelectors").getElementsByTagName("hbox");
+	for(i=0; i < colorList.length; i++){
+		var accountId = colorList.item(i).getAttribute("id");
+		var color = colorList.item(i).getElementsByTagName("colorpicker").item(0).getAttribute("color");
+		myProperties.setCharPref("colors."+accountId,color.toString());
+	}
+	var borderWidth = document.getElementById("BorderWidth").getAttribute("value");
+	myProperties.setCharPref("borderWidth",borderWidth);
+}
---
+++ chrome/content/overlay.js
@@ -0,0 +1,31 @@
+﻿  window.addEventListener("compose-window-init", function() {
+		colorBorder();
+	}, true);
+
+var accountSelector = document.getElementById("msgIdentity");
+accountSelector.setAttribute("oncommand","colorBorder();");
+
+function colorBorder(){
+	LoadIdentity(false);
+	//Retrieve the panel
+	var writingPanel = document.getElementById("headers-box").parentNode;
+	//Apply the border
+	var properties = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService);
+	var myProperties = properties.getBranch("extensions.borderColorsGT.");
+	var selectedIdentity = accountSelector.label;
+	var identityProperty = myProperties.getChildList("colors."+selectedIdentity);
+	var borderWidth =  myProperties.getChildList("borderWidth");
+	var BWidth;
+	if(borderWidth.length==0){
+		BWidth = 1;
+	}
+	else{
+		BWidth = myProperties.getCharPref("borderWidth");
+	}
+	if(identityProperty.length==0){
+		writingPanel.setAttribute("style","border: "+BWidth+"px dashed gray");
+	}
+	else{
+		writingPanel.setAttribute("style","border: "+BWidth+"px solid  "+myProperties.getCharPref("colors."+selectedIdentity));
+	}
+}
