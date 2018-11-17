3cbc92a38246c4148f92301bc812c84d00181304
dreadnaut <dreadnaut@gmail.com>
2018-11-17 15:17:56 +0000

    Refactor options UI javascript

--- chrome/content/options.js
+++ chrome/content/options.js
@@ -1,44 +1,70 @@
-function init() {
-    const prefs = new BorderColorsPrefs();
+const borderColorsPrefs = new BorderColorsPrefs();
 
-    var accountManager = Components.classes["@mozilla.org/messenger/account-manager;1"].getService(Components.interfaces.nsIMsgAccountManager);
-    var accountsList = accountManager.accounts;
-    for (var i = 0; i < accountsList.length; i++) {
-        var nextAccount = accountsList.queryElementAt(i, Components.interfaces.nsIMsgAccount);
-        var identitiesList = nextAccount.identities;
-        for (var j = 0; j < identitiesList.length; j++) {
-            var nextIdentity = identitiesList.queryElementAt(j, Components.interfaces.nsIMsgIdentity);
-            var newHbox = document.createElement("hbox");
-            var newLabel = document.createElement("label");
-            var newSeparator = document.createElement("separator");
-            var newColorPicker = document.createElement("colorpicker");
-            newLabel.setAttribute("value", nextIdentity.identityName);
-            newHbox.setAttribute("align", "center");
-            newHbox.setAttribute("id", nextIdentity.identityName);
-            newSeparator.setAttribute("orient", "vertical");
-            var container = document.getElementById('colorSelectors');
-            newColorPicker.setAttribute("type", "button");
-            newColorPicker.setAttribute("color", prefs.getColor(nextIdentity.identityName));
-            newHbox.appendChild(newColorPicker);
-            newHbox.appendChild(newSeparator);
-            newHbox.appendChild(newLabel);
-            container.appendChild(newHbox);
-        }
-    }
+
+function onLoad() {
+    var colorSelectors = document.getElementById('colorSelectors');
+    getAllIdentities()
+        .map((name) => createColorSelector(name, borderColorsPrefs.getColor(name)))
+        .map((row) => colorSelectors.appendChild(row));
 
     const sizeSelector = document.getElementById('BorderWidth');
-    sizeSelector.selectedIndex = prefs.getInt("borderWidth", 1) - 1;
+    sizeSelector.selectedIndex = borderColorsPrefs.getInt("borderWidth", 1) - 1;
 }
 
-function accept() {
-    const prefs = new BorderColorsPrefs();
-
+function onAccept() {
     var colorList = document.getElementById("colorSelectors").getElementsByTagName("hbox");
     for (i = 0; i < colorList.length; i++) {
         var accountId = colorList.item(i).getAttribute("id");
-        var color = colorList.item(i).getElementsByTagName("colorpicker").item(0).getAttribute("color");
-        prefs.setColor(accountId, color.toString());
+        var colorPicker = colorList.item(i).getElementsByTagName("colorpicker").item(0);
+        var color = colorPicker.getAttribute("color");
+        if (color != "transparent") {
+            borderColorsPrefs.setColor(accountId, color.toString());
+        }
     }
+
     var borderWidth = document.getElementById("BorderWidth").getAttribute("value");
-    prefs.setInt("borderWidth", borderWidth);
+    borderColorsPrefs.setInt("borderWidth", borderWidth);
+}
+
+function getAllIdentities() {
+    const accountManager = Components
+        .classes["@mozilla.org/messenger/account-manager;1"]
+        .getService(Components.interfaces.nsIMsgAccountManager);
+
+    const names = [];
+
+    for (let account of elementsOf(accountManager.accounts, 'nsIMsgAccount')) {
+        for (let identity of elementsOf(account.identities, 'nsIMsgIdentity')) {
+            names.push(identity.identityName);
+        }
+    }
+
+    return names;
+}
+
+function* elementsOf(collection, itemType) {
+    for (let i = 0; i < collection.length; i++) {
+        yield collection.queryElementAt(i, Components.interfaces[itemType]);
+    }
+}
+
+function createColorSelector(identity, color) {
+    var picker = document.createElement("colorpicker");
+    picker.setAttribute("type", "button");
+    picker.setAttribute("color", color);
+
+    var separator = document.createElement("separator");
+    separator.setAttribute("orient", "vertical");
+
+    var label = document.createElement("label");
+    label.setAttribute("value", identity);
+
+    var row = document.createElement("hbox");
+    row.setAttribute("align", "center");
+    row.setAttribute("id", identity);
+    row.appendChild(picker);
+    row.appendChild(separator);
+    row.appendChild(label);
+
+    return row;
 }
