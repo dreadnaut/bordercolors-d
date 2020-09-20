dbf00453079df153c65f246602a1c48aa2b168c9
dreadnaut <dreadnaut@gmail.com>
2020-09-20 12:42:20 +0100

    Add CSS for a few different styles

--- modules/styles.js
+++ modules/styles.js
@@ -13,23 +13,41 @@ export class Styles {
       + this.getHtmlStyle(styleId, color) + "}";
   }
 
+  get all() {
+    return [
+      { key: 'all-sides', label: 'All sides' },
+      { key: 'top-and-bottom', label: 'Top and bottom border' },
+      { key: 'top', label: 'Top border' },
+      { key: 'left-side-gradient', label: 'Gradient on the left side' },
+      { key: 'background-top-gradient', label: 'Gradient in the top-right corner' },
+      { key: 'background-bottom-gradient', label: 'Gradient at the bottom' },
+      { key: 'background', label: 'Background color' },
+    ];
+  }
+
   getHtmlStyle(styleId, color) {
+    const borderStyle = color 
+      ? `solid ${color}`
+      : `dashed ${this.defaultColor}`;
+
     switch (styleId) {
-      case "top-only":
-        return this.topOnly(color);
+      case "all-sides":
+        return `border: 10px ${borderStyle};`;
+      case "top-and-bottom":
+        return `border-top: 10px ${borderStyle}; border-bottom: 10px ${borderStyle};`;
+      case "top":
+        return `border-top: 10px ${borderStyle};`;
+      case "left-side-gradient":
+        return `border-left: 10px ${borderStyle}; border-image: linear-gradient(to bottom, ${color}, white 50%) 1 100%;`;
+      case "background":
+        return `background: ${color};`;
+      case "background-top-right-gradient":
+        return `background: linear-gradient(to bottom left, ${color}, transparent 20%);`;
+      case "background-bottom-gradient":
+        return `background: linear-gradient(to top, ${color}, white 20%);`;
       default:
-        return this.undefinedStyle();
+        return `border: 0.75rem dashed ${this.defaultColor};`
     }
-  }
-
-  topOnly(color) {
-    return color
-      ? `border-top: 10px solid ${color};`
-      : `border-top: 10px dashed ${this.defaultColor};`
-  }
-
-  undefinedStyle() {
-    return `border: 0.75rem dashed ${this.defaultColor};`
   }
 
 }
