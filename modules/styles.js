7793ce0b4a49886325778f4f76469a59953b9d72
dreadnaut <dreadnaut@gmail.com>
2020-12-21 19:54:38 +0000

    Add new style: gradient in all four corners

--- modules/styles.js
+++ modules/styles.js
@@ -20,6 +20,7 @@ export class Styles {
       { key: 'top', label: 'Top border' },
       { key: 'left-side-gradient', label: 'Gradient on the left side' },
       { key: 'background-top-right-gradient', label: 'Gradient in the top-right corner' },
+      { key: 'background-four-corners-gradient', label: 'Gradient in the four corners' },
       { key: 'background-bottom-gradient', label: 'Gradient at the bottom' },
       { key: 'background', label: 'Background color' },
     ];
@@ -45,6 +46,13 @@ export class Styles {
         return `body { background: ${color}; }`;
       case "background-top-right-gradient":
         return `body { background-image: linear-gradient(to bottom left, ${color}, transparent 20%); background-attachment: fixed; }`;
+      case "background-four-corners-gradient":
+        return `body { background-image:
+          linear-gradient( 45deg, ${color}, transparent 10%),
+          linear-gradient(135deg, ${color}, transparent 10%),
+          linear-gradient(225deg, ${color}, transparent 10%),
+          linear-gradient(315deg, ${color}, transparent 10%);
+          background-attachment: fixed; }`;
       case "background-bottom-gradient":
         return `body { background-image: linear-gradient(to top, ${color}, transparent 20%); background-attachment: fixed; }`;
       default:
